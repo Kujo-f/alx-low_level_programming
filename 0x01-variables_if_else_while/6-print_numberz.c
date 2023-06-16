c93f2376faeaabab035568d5661f9d379de04740
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-16 15:23:20 -0700

    file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+
+return (0);
+}
+
