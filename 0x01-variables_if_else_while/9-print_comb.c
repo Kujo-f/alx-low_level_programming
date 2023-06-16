8482d7fcf3e1d97987d1248f099f7395b14d6c74
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-16 15:53:07 -0700

    added file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
+
