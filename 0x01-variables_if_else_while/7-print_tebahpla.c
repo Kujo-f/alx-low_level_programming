262143dea6820c09788b3fe631685e1f33236a69
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-16 15:41:25 -0700

    added file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+
+return (0);
+}
+
