26fa214f1517834f9b5390ae71fe407a915d97f2
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-16 06:50:11 -0700

    added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letters;
+
+for (letters = 'a'; letters <= 'z'; letters++)
+{
+putchar(letters);
+}
+for (letters = 'A'; letters <= 'Z'; letters++)
+{
+putchar(letters);
+}
+
+putchar('\n');
+return (0);
+}
