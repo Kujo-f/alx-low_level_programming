6ab866a7f376cd07b228f4793f616903aa388935
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-16 06:18:50 -0700

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letters;
+for (letters = 'a'; letters <= 'z'; letters++)
+{
+putchar(letters);
+}
+{
+putchar('\n');
+}
+return (0);
+}
