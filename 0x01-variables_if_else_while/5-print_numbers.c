4dc1e89d2aa32ef1557d14e0c85aa85132ad2c53
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-16 08:53:23 -0700

    added 5-print_nubmers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entyr point of the program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+{
+printf("%d", number);
+}
+printf("\n");
+return (0);
+}
