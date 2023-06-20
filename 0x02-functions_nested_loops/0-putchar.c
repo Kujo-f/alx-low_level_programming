2e7bd0befd8bef1cc4a83ffa6d1f6ec724575fdf
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-20 02:20:45 -0700

    my first commit of the day

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+/* 
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+/*write each character*/
+{
+write(1, "_", 1);
+write(1, "p", 1);
+write(1, "u", 1);
+write(1, "t", 1);
+write(1, "c", 1);
+write(1, "h", 1);
+write(1, "a", 1);
+write(1, "r", 1);
+write(1, '\n', 1);
+return (0);
+}
