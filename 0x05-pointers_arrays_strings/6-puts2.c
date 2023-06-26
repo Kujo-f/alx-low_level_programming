3eb709e521c3767fff1dd4fb05b75be038c6f47d
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-26 13:51:00 -0700

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,9 @@ void puts2(char *str)
 
     while (str[i] != '\0')
     {
-        _putchar(str[i]);
-        i += 2;
+        if (i % 2 == 0)
+            _putchar(str[i]);
+        i++;
     }
 
     _putchar('\n');
