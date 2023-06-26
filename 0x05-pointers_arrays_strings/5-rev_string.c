7f2d7e73c188651a97bf9e1e835135bfbe0bdccb
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-26 13:24:31 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+    int length = 0;
+    int start, end;
+    char temp;
+
+    /* Calculate the length of the string */
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    /* Reverse the string using two pointers */
+    start = 0;
+    end = length - 1;
+
+    while (start < end)
+    {
+        temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+}
