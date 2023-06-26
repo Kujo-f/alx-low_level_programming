5406a90ec93bdb82af16d17994c0773e22e0b458
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-26 13:38:55 -0700

    task 6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
+/**
+ * function reverses the contents of a string
+ *
+ * return null
+ */
 void rev_string(char *s)
 {
     int length = 0;
     int start, end;
     char temp;
-
     /* Calculate the length of the string */
     while (s[length] != '\0')
     {
         length++;
     }
-
     /* Reverse the string using two pointers */
     start = 0;
     end = length - 1;
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * function prints every other character of a string,
+ *  starting with the first character,
+ *  followed by a new line.
+ */
+void puts2(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i += 2;
+    }
+
+    _putchar('\n');
+}
