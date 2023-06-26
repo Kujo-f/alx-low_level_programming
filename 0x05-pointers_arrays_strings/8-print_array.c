cdfcd07fc0ed590212d8e5fd00c16d91bbff39b7
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-26 14:30:44 -0700

    task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,42 @@
+# include "main.h"
+
+/**
+ * print_number - Prints a number
+ * @n: The number to print
+ *
+ * Description: Uses recursion to handle negative numbers
+ */
+void print_number(int n)
+{
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: The array of integers
+ * @n: The number of elements to print
+ *
+ * Description: Numbers are separated by comma and space, followed by a new line.
+ * The numbers should be displayed in the same order as they are stored in the array.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		print_number(a[i]);
+
+		/* Print comma and space for all elements except the last one */
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	_putchar('\n');
+}
