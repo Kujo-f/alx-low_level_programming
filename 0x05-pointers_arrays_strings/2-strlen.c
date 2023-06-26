5690b625a4c916233a1892678def2cbcc42de1c2
Kujo-f <cyberpunk2077t3@gmail.com>
2023-06-26 10:51:01 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - for finding the length of string
+ *
+ * Returns the value of the length of a string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0') 
+	{
+		length ++;
+		s++;
+	}
+	return length;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 int reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
