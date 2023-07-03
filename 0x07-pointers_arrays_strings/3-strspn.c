03cd056c799e2d1729a5953b0562ac1e5e5ddb29
Kujo-f <cyberpunk2077t3@gmail.com>
2023-07-03 04:58:15 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string.
+ * @accept: Pointer to the string containing the accepted characters.
+ *
+ * Return: Number of bytes in the initial segment of s consisting only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+	int isAccepted;
+
+	while (*s != '\0')
+	{
+		isAccepted = 0;
+
+		/* Iterate through the accept string to check if *s is accepted */
+		char *a = accept;
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				isAccepted = 1;
+				break;
+			}
+			a++;
+		}
+
+		if (isAccepted)
+			length++;
+		else
+			break;
+
+		s++;
+	}
+
+	return length;
+}
