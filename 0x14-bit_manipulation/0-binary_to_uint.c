c503619886cded61f335b77c17f8006977eb3aaf
Kujo-f <opiyokelly525@gmail.com>
2023-08-03 14:59:17 -0700

    added changes to task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,21 @@
  *
  * returns converted number on success
  */
-unsigned int binary_to_int(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-     int n;
-     unsigned int dec_val = 0;
+	int n;
+	unsigned int dec_val = 0;
 
-     if (!b)
-	    return (0);
-     
-     for (n = 0; b[n]; n++)
-     {
-	    if (b[n] < '0' || b[n] > '1')
-		    return (0);
-	    dec_val = 2 * dec_val + (b[n] - '0');
-     }
+	if (!b)
+		return (0);
 
-     return (dec_val);
+	for (n = 0; b[n]; n++)
+	{
+		if (b[n] < '0' || b[n] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[n] - '0');
+	}
+
+	return (dec_val);
 }
+
